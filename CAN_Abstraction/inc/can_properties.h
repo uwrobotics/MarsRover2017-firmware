898cb4fcf10ca17af21e675e37b75dc10e7d5c88
RyanSamlalsingh <ryan.samlalsingh@gmail.com>
2016-09-04 11:30:21 -0400

    Switch to new laptop, also restarting development. Might be broken

--- CAN_Abstraction/inc/can_properties.h
+++ CAN_Abstraction/inc/can_properties.h
@@ -37,15 +37,39 @@
 #define CAN_IDE_TYPE			CAN_ID_STD
 #define CAN_RTR_TYPE			CAN_RTR_DATA
 
-#define CAN_ABSTRACTION_PRIMITIVE_INT8			(uint8_t)0
-#define CAN_ABSTRACTION_PRIMITIVE_INT32 		(uint8_t)1
-#define CAN_ABSTRACTION_PRIMITIVE_INT64_LSB		(uint8_t)2
-#define CAN_ABSTRACTION_PRIMITIVE_INT64_MSB		(uint8_t)3
-#define CAN_ABSTRACTION_PRIMITIVE_CHAR  		(uint8_t)5
+#define CAN_ABSTRACTION_TYPE_INT8			(uint8_t)0
+#define CAN_ABSTRACTION_TYPE_UINT32 		(uint8_t)1
+#define CAN_ABSTRACTION_TYPE_UINT64_LSB		(uint8_t)2
+#define CAN_ABSTRACTION_TYPE_UINT64_MSB		(uint8_t)3
+#define CAN_ABSTRACTION_TYPE_FLOAT			(uint8_t)4
+#define CAN_ABSTRACTION_TYPE_DOUBLE			(uint8_t)5
+#define CAN_ABSTRACTION_TYPE_CHAR  			(uint8_t)10
+#define CAN_ABSTRACTION_TYPE_SHORT_STRING	(uint8_t)11
+#define CAN_ABSTRACTION_TYPE_LONG_STRING	(uint8_t)12
 
-#define CAN_ABSTRACTION_UNSIGNED	(uint8_t)0
-#define CAN_ABSTRACTION_SIGNED		(uint8_t)1
+#define CAN_ABSTRACTION_NEGATIVE				(uint8_t)16
 
-void CANAbstract_Transmit_Uint(uint32_t message);
+typedef enum{
+	POSITIVE=0,
+	NEGATIVE = 16
+} sign_enum;
+
+typedef union{
+	uint32_t result;
+	float input;
+} float_union;
+
+typedef struct{
+	uint8_t return_type;
+	union{
+		int64_t return_int;
+		uint64_t return_uint;
+		char return_char;
+		float return_float;
+		char** return_string_ptr;
+	};
+} return_struct;
+
+void CANAbstract_Transmit_Uint(uint64_t message);
 
 #endif
--- CAN_Abstraction/src/can_abstraction.c
+++ CAN_Abstraction/src/can_abstraction.c
@@ -6,6 +6,8 @@
  */
 
 #include "can_properties.h"
+#include <limits.h>
+#include <string.h>
 
 
 GPIO_InitTypeDef GPIO_InitStruct;
@@ -67,45 +69,125 @@ void CANAbstract_ClearDataArray(){
 	}
 }
 
-void CANAbstract_Transmit_Uint(uint64_t message){
+void Internal_CANAbstract_Transmit_WholeNumber(uint64_t message, sign_enum sign){
 
 	CANAbstract_ClearDataArray();
+
+	CAN_HandleStruct.pTxMsg->DLC = 0;
 
 	CAN_HandleStruct.pTxMsg->Data[1] = message & 225;
 	CAN_HandleStruct.pTxMsg->Data[2] = (message >> 8) & 225;
 	CAN_HandleStruct.pTxMsg->Data[3] = (message >> 16) & 225;
 	CAN_HandleStruct.pTxMsg->Data[4] = (message >> 24) & 225;
-	CAN_HandleStruct.pTxMsg->Data[5] = CAN_ABSTRACTION_UNSIGNED;
-	if (message <= 4294967295){
-		CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_PRIMITIVE_INT32;
-		CAN_HandleStruct.pTxMsg->Data[5] = CAN_ABSTRACTION_UNSIGNED;
-		CAN_HandleStruct.pTxMsg->DLC = 0;
+	if (message <= ULONG_MAX){
+
+		CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_TYPE_UINT32 | sign;
 		HAL_CAN_Transmit_IT(&CAN_HandleStruct);
+
 	} else {
-		CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_PRIMITIVE_INT64_LSB;
 
-		CAN_HandleStruct.pTxMsg->Data[5] = CAN_ABSTRACTION_UNSIGNED;
-		CAN_HandleStruct.pTxMsg->DLC = 0;
+		CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_TYPE_UINT64_LSB | sign;
 		HAL_CAN_Transmit_IT(&CAN_HandleStruct);
 
-		CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_PRIMITIVE_INT64_MSB;
+		CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_TYPE_UINT64_MSB | sign;
 		CAN_HandleStruct.pTxMsg->Data[1] = message & 225;
-		CAN_HandleStruct.pTxMsg->Data[2] = (message >> 8) & 225;
-		CAN_HandleStruct.pTxMsg->Data[3] = (message >> 16) & 225;
-		CAN_HandleStruct.pTxMsg->Data[4] = (message >> 24) & 225;
+		CAN_HandleStruct.pTxMsg->Data[2] = (message >> 32) & 225;
+		CAN_HandleStruct.pTxMsg->Data[3] = (message >> 40) & 225;
+		CAN_HandleStruct.pTxMsg->Data[4] = (message >> 48) & 225;
 
 		HAL_CAN_Transmit_IT(&CAN_HandleStruct);
+
 	}
 }
 
-void CANAbstract_Transmit_Int(int64_t message){
-
+void CANAbstract_Transmit_Uint(uint64_t message){
+	Internal_CANAbstract_Transmit_WholeNumber(message, POSITIVE);
 }
 
+void CANAbstract_Transmit_Int(int64_t message){
+	CANAbstract_ClearDataArray();
+
+	if (message < 0){
+		Internal_CANAbstract_Transmit_WholeNumber((uint64_t)(-message), NEGATIVE);
+	} else {
+		Internal_CANAbstract_Transmit_WholeNumber((uint64_t)(message), POSITIVE);
+	}
+}
+
+void CANAbstract_Transmit_Float(float message){
+	//Done according to floating point standards:
+	//	Data[1] has the sign bit
+	//  Data[2] has the exponent
+	//  Data[3-5] has the fraction from LSBs to MSBs
+	CANAbstract_ClearDataArray();
+
+	float_union this_float_union;
+	this_float_union.input = message;
+
+	CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_TYPE_FLOAT;
+	CAN_HandleStruct.pTxMsg->Data[1] = (this_float_union.result >> 31) & 1;
+	CAN_HandleStruct.pTxMsg->Data[2] = (this_float_union.result >> 23) & 255;
+	CAN_HandleStruct.pTxMsg->Data[3] = this_float_union.result & 255;
+	CAN_HandleStruct.pTxMsg->Data[4] = (this_float_union.result >> 8) & 255;
+	CAN_HandleStruct.pTxMsg->Data[5] = (this_float_union.result >> 16) & 127;
+
+	CAN_HandleStruct.pTxMsg->DLC = 0;
+
+	HAL_CAN_Transmit_IT(&CAN_HandleStruct);
+}
+
+
 void CANAbstract_Transmit_Char(char message){
-	CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_PRIMITIVE_CHAR;
+	CANAbstract_ClearDataArray();
+	CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_TYPE_CHAR;
 	CAN_HandleStruct.pTxMsg->Data[1] = (uint8_t)message;
 	CAN_HandleStruct.pTxMsg->DLC = 0;
 	HAL_CAN_Transmit_IT(&CAN_HandleStruct);
 }
+
+void CANAbstract_Transmit_String(char message[]){
+	CANAbstract_ClearDataArray();
+
+	uint32_t message_length = strlen(message);
+	if (message_length <= 7){
+		CAN_HandleStruct.pTxMsg->Data[0] = CAN_ABSTRACTION_TYPE_SHORT_STRING;
+		for(int index = 1; index <= message_length; index++){
+			CAN_HandleStruct.pTxMsg->Data[index] = (uint8_t)message[index-1];
+		}
+
+		CAN_HandleStruct.pTxMsg->DLC = 0;
+		HAL_CAN_Transmit_IT(&CAN_HandleStruct);
+
+	}
+}
+
+return_struct CANAbstract_Receive_Decode(){
+	return_struct decoded_message;
+
+	decoded_message.return_type = CAN_HandleStruct.pRxMsg->Data[0];
+	switch (decoded_message.return_type){
+		case CAN_ABSTRACTION_TYPE_UINT32:
+			break;
+		case CAN_ABSTRACTION_TYPE_UINT64_LSB:
+			break;
+		case CAN_ABSTRACTION_TYPE_UINT64_MSB:
+			break;
+		case CAN_ABSTRACTION_TYPE_FLOAT:
+			break;
+		case CAN_ABSTRACTION_TYPE_DOUBLE:
+			break;
+		case CAN_ABSTRACTION_TYPE_CHAR:
+			break;
+		case CAN_ABSTRACTION_TYPE_SHORT_STRING:
+			break;
+		case CAN_ABSTRACTION_TYPE_LONG_STRING:
+			break;
+		default:
+			break;
+	}
+
+	return decoded_message;
+}
+
+
 
