f2000aec02cd8e3658ccb9aa155dd4ec92621bf7
lijerryli <jerry.li@uwaterloo.ca>
2017-01-24 16:55:32 -0500

    remove build and object files

--- stm32f072b-disco_hal_lib/Debug/Utilities/Components/hx8347d/hx8347d.d
+++
@@ -1,8 +0,0 @@
-Utilities/Components/hx8347d/hx8347d.o: \
- ../Utilities/Components/hx8347d/hx8347d.c \
- ../Utilities/Components/hx8347d/hx8347d.h \
- ../Utilities/Components/hx8347d/../Common/lcd.h
-
-../Utilities/Components/hx8347d/hx8347d.h:
-
-../Utilities/Components/hx8347d/../Common/lcd.h:
--- stm32f072b-disco_hal_lib/Debug/Utilities/Components/stlm75/stlm75.d
+++
@@ -1,8 +0,0 @@
-Utilities/Components/stlm75/stlm75.o: \
- ../Utilities/Components/stlm75/stlm75.c \
- ../Utilities/Components/stlm75/stlm75.h \
- ../Utilities/Components/stlm75/../Common/tsensor.h
-
-../Utilities/Components/stlm75/stlm75.h:
-
-../Utilities/Components/stlm75/../Common/tsensor.h:
