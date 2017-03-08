da11cc73b5ac4bd56b133d1a9da63f66a1c2ec24
archielee <archie_lee7@hotmail.com>
2017-03-07 21:04:37 -0500

    Removed giant comment block

--- pwmlib/inc/pwmlib.h
+++ pwmlib/inc/pwmlib.h
@@ -30,37 +30,6 @@ Copyright 2017, UW Robotics Team
 #define PWM4_GPIO_ALTERNATE         GPIO_AF1_TIM3
 
 // PWM timer configuration
-  /* -----------------------------------------------------------------------
-  TIM2 Configuration: generate 4 PWM signals with 4 different duty cycles.
-
-    In this example TIM2 input clock (TIM2CLK) is set to APB1 clock (PCLK1),
-    since APB1 prescaler is equal to 1.
-      TIM2CLK = PCLK1
-      PCLK1 = HCLK
-      => TIM2CLK = HCLK = SystemCoreClock
-
-    To get TIM2 counter clock at 16 MHz, the prescaler is computed as follows:
-       Prescaler = (TIM2CLK / TIM2 counter clock) - 1
-       Prescaler = ((SystemCoreClock) /16 MHz) - 1
-
-    To get TIM2 output clock at 24 KHz, the period (ARR)) is computed as follows:
-       ARR = (TIM2 counter clock / TIM2 output clock) - 1
-           = 665
-
-    TIM2 Channel1 duty cycle = (TIM2_CCR1/ TIM2_ARR + 1)* 100 = 50%
-    TIM2 Channel2 duty cycle = (TIM2_CCR2/ TIM2_ARR + 1)* 100 = 37.5%
-    TIM2 Channel3 duty cycle = (TIM2_CCR3/ TIM2_ARR + 1)* 100 = 25%
-    TIM2 Channel4 duty cycle = (TIM2_CCR4/ TIM2_ARR + 1)* 100 = 12.5%
-
-    Note:
-     SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f0xx.c file.
-     Each time the core clock (HCLK) changes, user had to update SystemCoreClock
-     variable value. Otherwise, any configuration based on this variable will be incorrect.
-     This variable is updated in three ways:
-      1) by calling CMSIS function SystemCoreClockUpdate()
-      2) by calling HAL API function HAL_RCC_GetSysClockFreq()
-      3) each time HAL_RCC_ClockConfig() is called to configure the system clock frequency
-  ----------------------------------------------------------------------- */
 #define PWM_PRESCALER               47      // System clock frequency / Timer frequency - 1
 #define PWM_PERIOD                  19999   // Timer frequency / PWM frequency - 1
 #define PWM_CLOCK_DIVISION          TIM_CLOCKDIVISION_DIV1
