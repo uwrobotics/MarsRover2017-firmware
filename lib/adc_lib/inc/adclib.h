5b8810f394e3d70df953339d8ba62ab3fc34341e
Ivy Xing <r5xing@uwaterloo.ca>
2017-05-11 18:47:21 -0400

    Added ADC library

---
+++ lib/adc_lib/inc/adclib.h
@@ -0,0 +1,24 @@
+/*************************************************
+
+@file     adclib.h
+@author   Ivy Xing
+
+How to use this library:
+- Initialize the ADC pin with ADC_Init()
+- Read values from the pin with ADC_Read()
+
+Note:
+- Supported ADC pins: PA1, PA2, PA3, PC1
+
+*************************************************/
+
+#ifndef ADCLIB_H_
+#define ADCLIB_H_
+
+#include "stm32f0xx.h"
+
+//Public Functions
+int ADC_Init(uint32_t adc_id);
+int ADC_Read(uint32_t adc_id);
+
+#endif
