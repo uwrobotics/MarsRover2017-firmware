3fdaf19f145390b439d73f18e802ed3df3c0c9cb
orodrigu <oswinrods@gmail.com>
2017-05-13 23:42:17 -0400

    initial code for uv_test

---
+++ app/uv_test/src/uv_test.c
@@ -0,0 +1,69 @@
+/*
+ * Testing UV sensor code
+*/
+
+#include "stm32f0xx.h"
+#include "pins.h"
+#include "i2clib.h"
+#include "uv.h"
+
+static const uint16_t TIMEOUT = 5000;
+
+static void Error_Handler(void);
+void CLK_Init(void);
+
+int main(void)
+{
+    HAL_Init();
+    CLK_Init();
+    GPIO_Init();
+	I2C_init(I2C1);
+
+	I2C_Device_t uv_sensor;
+	uv_int_time_t int_time = ONE;
+	init_uv(&uv_sensor, ONE, TIMEOUT);
+
+	uint16_t uv_data = 0;
+
+	while(1) {
+		read_uv(&uv_sensor, &uv_data);
+		printf(uv_data);
+		HAL_Delay(500);
+	}
+
+    return 0;
+}
+
+static void Error_Handler(void)
+{
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+    while (1) {}
+}
+
+void CLK_Init(void)
+{
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+
+    /* Select HSI48 Oscillator as PLL source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+    RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI48;
+    RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /* Select PLL as system clock source and configure the HCLK and PCLK1 clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1)!= HAL_OK)
+    {
+        Error_Handler();
+    }
+}
