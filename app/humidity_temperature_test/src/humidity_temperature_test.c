7a88a990ae24399cba69e5997157b5493cb61faa
orodrigu <oswinrods@gmail.com>
2017-05-19 00:15:23 -0400

    correct way to tx-ing

--- app/humidity_temperature_test/src/humidity_temperature_test.c
+++ app/humidity_temperature_test/src/humidity_temperature_test.c
@@ -5,20 +5,19 @@
 #include "stm32f0xx.h"
 // #include "i2clib.h"
 // #include "humidity_temperature.h"
+#include "uart_lib.h"
 
 static const uint16_t TIMEOUT = 5000;
 
 static void Error_Handler(void);
 void CLK_Init(void);
-void TX_Init(void);
-void TX_data(float data);
 
 int main(void)
 {
     HAL_Init();
     CLK_Init();
 	// I2C_init(I2C1);
-    TX_Init();
+    UART_LIB_INIT();
 
 	// HT_Device_t ht_sensor;
 	// init_ht(&ht_sensor, TIMEOUT);
@@ -29,9 +28,12 @@ int main(void)
 	while(1) {
 		// humidity = read_hum(&ht_sensor);
         // temperature = read_temp(&ht_sensor);
-		// display data somehow
+		
+        // display data somehow
         humidity += 1;
         temperature += 2;
+        UART_LIB_PRINT_DOUBLE(humidity);
+        UART_LIB_PRINT_DOUBLE(temperature);
 
 		HAL_Delay(500);
 	}
@@ -71,45 +73,4 @@ void CLK_Init(void)
     {
         Error_Handler();
     }
-}
-
-void TX_Init(void)
-{   /*
-     * See below link for code comments and explanations
-     * stm32f4-discovery.net/2014/04/library-04-connect-stm32f429-discovery-to-computer-with-usart/
-    */
-
-    // Initialize PA9
-
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
-
-    GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-    // Initialize USART
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
- 
-    USART_InitTypeDef USART_InitStruct;
-    USART_InitStruct.USART_BaudRate = baudrate;
-    USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStruct.USART_Mode = USART_Mode_Tx;
-    USART_InitStruct.USART_Parity = USART_Parity_No;
-    USART_InitStruct.USART_StopBits = USART_StopBits_1;
-    USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-    USART_Init(USART1, &USART_InitStruct);
-    USART_Cmd(USART1, ENABLE);
-}
-
-void TX_data(float data)
-{
-    while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-    USART_SendData(USART1, data);
 }
